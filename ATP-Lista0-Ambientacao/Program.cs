4937fa0368e38ef7f1b7649d42f1e482e194957e
Sara da Cruz Freitas <159440011+sahfreitas@users.noreply.github.com>
2026-03-02 20:24:31 -0300

    Add files via upload

---
+++ ATP-Lista0-Ambientacao/Program.cs
@@ -0,0 +1,40 @@
+﻿// See https://aka.ms/new-console-template for more information
+/*Console.WriteLine("oieee")
+
+
+
+Console.Write("EXEMPLO 2 - Programa em C# que mostra algumas variações do comando");
+Console.Write("\n Os caracteres: Barra invertida + n - \n quebra a linha");
+Console.WriteLine("\n Pronto. Terminou. Saindo ... ");
+
+Console.WriteLine("EXEMPLO 3 -Programa em C#");
+Console.WriteLine(); // O que esse comando faz acontecer na tela de saída ?
+Console.WriteLine("Olá ! Eu sou o {0} e tenho {1} anos de idade. Bem-vindo !", "Sara", 17);
+
+int a, b, c, d, X;
+Console.WriteLine("EXEMPLO 4 - Programa em C#");
+Console.WriteLine("Entre com um valor qualquer de 4 algarismos: ");
+X = int.Parse(Console.ReadLine());
+a = X / 1000;
+b = (X % 1000) / 100;
+c = ((X % 1000) % 100)/ 10;
+d = ((X % 1000) % 100) % 10;
+Console.WriteLine("{0}{1}{2}{3}",d, c, b, a);
+Console.WriteLine();
+
+int num1, num2, soma, ;
+
+Console.WriteLine("Digite um número:");
+num1 = int.Parse(Console.ReadLine());
+Console.WriteLine("Digite um número:");
+num2 = int.Parse(Console.ReadLine());
+soma = num1 + num2;
+Console.WriteLine("Soma: {0}", soma);*/
+
+int x, y;
+Console.WriteLine("Digite um número:");
+x = int.Parse(Console.ReadLine());
+y = 2 * x;
+Console.WriteLine("Dobro: " + y);
+Console.ReadKey();
+
